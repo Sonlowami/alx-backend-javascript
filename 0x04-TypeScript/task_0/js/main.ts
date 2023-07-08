5333cda9bcf2c690b00afa3aad5f371ec7efaf34
Sonlowami <uwilowami64@outlook.com>
2023-07-08 23:31:21 +0200

    Solve task 8

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -8,24 +8,28 @@ interface Student {
 const student1: Student = {
   firstName: 'Eric',
   lastName: 'Loi',
-  age: '52',
+  age: 52,
   location: 'Jakarta'
 }
 
 const student2: Student = {
   firstName: 'Emily',
   lastName: 'Rowe',
-  age: '20',
+  age: 20,
   location: 'UK'
 }
 
 const studentsList: Student[] = [student1, student2];
 const table = document.createElement('table');
-table.append(document.createElement('th').innerHTML('Student'),
-  document.createElement('th').innerHTML('Location'));
+const th1 = document.createElement('th');
+th1.innerText = 'Students';
+const th2 = document.createElement('th');
+th2.innerText = 'Location';
+table.append(th1, th2);
 
-for item of studentList:
-  table.append(document.createElement('tr').innerHTML(
-    item.firstName, item.lastName));
+for (let learner of studentsList) {
+  let row = document.createElement('tr')
+  row.innerHTML = `${learner.firstName}, ${learner.location}`
+}
 
 document.body.append(table);
